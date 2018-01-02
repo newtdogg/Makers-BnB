fe6fbae21786ea048ddfb57006792ff65fab7367
Lars <larsfinlay@googlemail.com>
2018-01-02 18:16:09 +0000

    Test alteration

--- makersbnb/tests/featureTests/appInfranstructure.js
+++ makersbnb/tests/featureTests/appInfranstructure.js
@@ -5,7 +5,28 @@ var Browser = require('zombie');
 var assert = require('assert');
 
   describe('home page', function() {
-    it('should load the home page', function() {
-      assert.ok(this.browser.success);
+
+    before(function() {
+      server = app.listen(3000)
+      this.browser = new Browser({ site: 'http://localhost:3000' });
     });
+
+    after(done => server.close(done));
+
+    describe('pretesting', function(done) {
+
+      before(function(done) {
+        this.browser.visit('/', done);
+      })
+
+      it('should load the home page', function() {
+        assert.ok(this.browser.success);
+      });
+
+      it('should see express', function() {
+        assert.equal(this.browser.assert.text('h1'), 'Express');
+      });
+
+    });
+
   });
