87a6be2cf1c0050cfc69a3b5b78b1e188f45ed8a
James Lizamore <jwlizamore@gmail.com>
2018-01-03 17:21:56 +0000

    Fixed test issue, truncating tables

--- makersbnb/spec/featureTests/appInfranstructure.js
+++ makersbnb/spec/featureTests/appInfranstructure.js
@@ -3,19 +3,24 @@ var app = require('../../app');
 // use zombie.js as headless browser
 var Browser = require('zombie');
 var assert = require('assert');
+var models = require('../../models')
+var DatabaseCleaner = require('database-cleaner')
+var databaseCleaner = new DatabaseCleaner('Postgres');
 
   describe('home page', function() {
+    // databaseCleaner.clean('airbnb_test');
 
     before(function() {
       server = app.listen(3000)
       browser = new Browser({ site: 'http://localhost:3000' });
     });
 
-    after(done => server.close(done));
+    //after(done => server.close(done));
 
     describe('pretesting', function(done) {
 
       before(function(done) {
+        models.Listing.truncate();
         browser.visit('/', done);
       })
 
@@ -27,12 +32,16 @@ var assert = require('assert');
         assert.equal(browser.text('form label'), 'Location: Price/Night: Guests:');
       });
 
-      it('should allow the user to upload a property', function() {
+      it('should allow the user to upload a property', function(done) {
+        // PressButton returns promise, to test asynch functions we must force
+        // wait using done(), which prevents chain of execution continuing.
         browser.fill('#loco', 'London')
         browser.fill('#price', '50')
         browser.fill('#guests', '3')
-        // browser.document.forms[0].submit()
-        assert.equal(browser.text('li'), 'Location - London | Price/Night - £50 | Guests - 3');
+        browser.pressButton('Submit', function(){
+           assert.equal(browser.text('li'), 'Location - London | Price/Night - £50 | Guests - 3');
+          done();
+        })
       });
 
     });
