fc6b5033b674e8b3ef0c1a61292a04d7c0a5cce5
newtdogg <edwardfrancisnewton@live.com>
2018-01-08 09:59:14 +0000

    sorted conflicts

--- makersbnb/lib/currentUser.js
+++ makersbnb/lib/currentUser.js
@@ -12,14 +12,11 @@ UserSession = function() {
     this.currentUser = null
   };
 
-<<<<<<< HEAD
   this.currentUserSearch = function(search) {
     this.search = search
   }
 
-=======
   this.hasCurrentUser = function(){
     return !(this.currentUser === null)
   }
->>>>>>> dbWork2
 }
