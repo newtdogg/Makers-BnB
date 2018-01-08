27232747e470fb86f827cea605bf53cd2b57a0d7
newtdogg <edwardfrancisnewton@live.com>
2018-01-08 12:03:07 +0000

    refactored filtering, now DRY

--- makersbnb/routes/index.js
+++ makersbnb/routes/index.js
@@ -6,9 +6,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var sessionUsername = null
   userSession.currentUser === null ? sessionUsername = "FLAG" : sessionUsername = userSession.currentUser.username
-  models.Listing.findAll().then(function(items) {
-    res.render('index', { title: 'JTLN', listings: items, userName: sessionUsername });
-  })
+  console.log(req.session.location )
+  if (req.session.location === null) {
+    models.Listing.findAll().then(function(items) {
+      res.render('index', { title: 'JTLN', listings: items, userName: sessionUsername });
+    })
+  } else {
+    models.Listing.findAll({where: {location: req.session.location}}).then(function(items) {
+      console.log(req.session.location)
+      req.session.location = null
+      res.render('index', { title: 'JTLN', listings: items, userName: sessionUsername });
+    })
+  }
+});
+
+router.post('/search', function(req,res,next) {
+  req.session.location = req.body.searchLocation
+  res.redirect('/#page-section2')
 });
 
 module.exports = router;
